31316ddfd666443d3460e7427c37dffc75338418
cocox888 <adlerhudsonmaf575@gmail.com>
2025-02-20 20:15:04 -0600

    start with test

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -1,13 +1,13 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.18;
 
-import {Test} from "forge-std/Test.sol";
+import {Test, console} from "forge-std/Test.sol";
 contract FundMeTest is Test {
     uint256 public number=1;
     function setUp() external{
         number=2;
     }
-    functino demoTest() public {
+    function testDemo() public view{
         assertEq(number, 2);
     }
 }
